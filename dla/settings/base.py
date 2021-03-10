7616aed18ef3213e7828c0ea3c97321b009b706c
Kaisar <kaucap.bm94@gmail.com>
2021-03-10 15:38:31 +0600

    SDASDAS

--- dla/settings/base.py
+++ dla/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'vh78)p-*-8-15)(a^=#8j-gy465(4sart)=akw1z@vr)2cq*j('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
